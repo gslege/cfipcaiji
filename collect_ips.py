8b5bb0fe98de7b241e91b4acf14976f4cef8a7cd
GSlege <91965195+gslege@users.noreply.github.com>
2025-08-12 01:10:13 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -13,7 +13,7 @@ urls = [
     'https://cf-ip.cdtools.click/shanghai', 
     'https://cf-ip.cdtools.click/chengdu', 
     'https://cf-ip.cdtools.click/shenzhen',
-    'https://www.cloudflare.com/ips-v4'
+    'https://www.cloudflare.com/zh-cn/ips'
 ]
 
 # 正则表达式用于匹配IP地址
