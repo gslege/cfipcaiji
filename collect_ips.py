8a0c928f14ba1e4c5e2c18c1b5fcb86a9db8eee9
GSlege <91965195+gslege@users.noreply.github.com>
2025-08-12 00:55:25 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -6,10 +6,13 @@ import os
 # 目标URL列表
 urls = [
     'https://ip.164746.xyz', 
-    'https://cf-ip.cdtools.click/shenzhen', 
     'https://stock.hostmonit.com/CloudFlareYes',
     'https://www.wetest.vip/page/cloudflare/address_v4.html',
-    'https://api.uouin.com/cloudflare.html'
+    'https://api.uouin.com/cloudflare.html', 
+    'https://cf-ip.cdtools.click/beijing', 
+    'https://cf-ip.cdtools.click/shanghai', 
+    'https://cf-ip.cdtools.click/chengdu', 
+    'https://cf-ip.cdtools.click/shenzhen'
 ]
 
 # 正则表达式用于匹配IP地址
