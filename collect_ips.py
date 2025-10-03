5d5bd9adbf8ba7615c7c03604bbfd0a59459eab2
GSlege <91965195+gslege@users.noreply.github.com>
2025-10-04 06:51:02 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -5,15 +5,15 @@ import os
 
 # 目标URL列表
 urls = [
-    'https://ip.164746.xyz', 
-    'https://stock.hostmonit.com/CloudFlareYes',
-    'https://www.wetest.vip/page/cloudflare/address_v4.html',
-    'https://api.uouin.com/cloudflare.html', 
-    'https://raw.githubusercontent.com/xingpingcn/enhanced-FaaS-in-China/refs/heads/main/Cf.json', 
-    'https://cf-ip.cdtools.click/beijing', 
-    'https://cf-ip.cdtools.click/shanghai', 
-    'https://cf-ip.cdtools.click/chengdu', 
-    'https://cf-ip.cdtools.click/shenzhen'
+    "https://ip.164746.xyz",
+    "https://www.wetest.vip/page/cloudflare/address_v4.html",
+    "https://api.uouin.com/cloudflare.html",
+    "https://addressesapi.090227.xyz/ip.164746.xyz",
+    "https://raw.githubusercontent.com/xingpingcn/enhanced-FaaS-in-China/refs/heads/main/Cf.json",
+    "https://cf-ip.cdtools.click/beijing",
+    "https://cf-ip.cdtools.click/shanghai",
+    "https://cf-ip.cdtools.click/chengdu",
+    "https://cf-ip.cdtools.click/shenzhen",
 ]
 
 # 正则表达式用于匹配IP地址
